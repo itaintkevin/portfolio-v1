57c545645c1f7cb5fd843afde02722dd43eb150c
itaintkevin <kpjtofficial@gmail.com>
2022-09-24 21:38:13 +0530

    Added Social Links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,9 +41,9 @@ const Hero = () => {
         </div>
     </div> */}
     <div className='relative flex flex-col items-center'>
-      <div className='absolute top-0 -left-4 w-72 h-72 rounded-full mix-blend-screen animate-blob opacity-20 filter blur-3xl bg-[#7F7FD5]'></div>
-      <div className='absolute top-0 -right-4 w-72 h-72 rounded-full mix-blend-screen animate-blob opacity-20 filter blur-3xl bg-[#91EAE4]'></div>
-      <div className='absolute left-10 -bottom-8 w-72 h-72 rounded-full mix-blend-screen animate-blob opacity-20 filter blur-3xl bg-[#86A8E7]'></div>
+      <div className='absolute top-0 -left-4 w-72 h-72 rounded-full mix-blend-color-dodge animate-blob opacity-30 filter blur-3xl bg-[#7F7FD5]'></div>
+      <div className='absolute top-0 -right-4 w-72 h-72 rounded-full mix-blend-color-dodge animate-blob opacity-30 filter blur-3xl bg-[#91EAE4]'></div>
+      <div className='absolute left-10 -bottom-8 w-72 h-72 rounded-full mix-blend-color-dodge animate-blob opacity-30 filter blur-3xl bg-[#86A8E7]'></div>
       <div><img className="rounded-full w-48 h-48 drop-shadow-2xl my-5" src={Profile} alt="Profile" /></div>
       <div className='text-4xl'>Hey!</div>
       <div className='text-5xl from-[#7F7FD5] via-[#86A8E7] to-[#91EAE4] text-transparent bg-clip-text bg-gradient-to-r drop-shadow-2xl'>I'm Kevin Paul</div>
@@ -51,7 +51,7 @@ const Hero = () => {
         <a className='flex items-center' href="https://github.com/itaintkevin"><AiOutlineGithub/>&nbsp;Github</a>
         <a className='flex items-center' href="https://www.linkedin.com/in/itaintkevin"><AiOutlineLinkedin/>&nbsp;Linkedin</a>
         <a className='flex items-center' href="https://twitter.com/it_aint_kevin"><AiOutlineTwitter/>&nbsp;Twitter</a>
-        <a className='flex items-center' href="https://www.instagram.com/it_aint_kevin"><AiOutlineInstagram/>&nbsp;Instagram</a>
+        <a className='flex items-center' href="><AiOutlineInstagram/>&nbsp;Instagram</a>
       </div>
       <div className='flex justify-center w-full my-5'>
         <div className='bg-[#91EAE4] absolute text-md rounded-xl mx-5 p-5'>
