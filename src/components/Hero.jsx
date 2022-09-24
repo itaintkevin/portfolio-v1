1d4106141c966b71267f9c5f213feb7b48d5e05f
itaintkevin <kpjtofficial@gmail.com>
2022-09-24 21:22:51 +0530

    Added Social Links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,15 +43,15 @@ const Hero = () => {
     <div className='relative flex flex-col items-center'>
       <div className='absolute top-0 -left-4 w-72 h-72 rounded-full mix-blend-screen animate-blob opacity-20 filter blur-3xl bg-[#7F7FD5]'></div>
       <div className='absolute top-0 -right-4 w-72 h-72 rounded-full mix-blend-screen animate-blob opacity-20 filter blur-3xl bg-[#91EAE4]'></div>
-      <div className='absolute left-20 -bottom-8 w-72 h-72 rounded-full mix-blend-screen animate-blob opacity-20 filter blur-3xl bg-[#86A8E7]'></div>
+      <div className='absolute left-10 -bottom-8 w-72 h-72 rounded-full mix-blend-screen animate-blob opacity-20 filter blur-3xl bg-[#86A8E7]'></div>
       <div><img className="rounded-full w-48 h-48 drop-shadow-2xl my-5" src={Profile} alt="Profile" /></div>
       <div className='text-4xl'>Hey!</div>
       <div className='text-5xl from-[#7F7FD5] via-[#86A8E7] to-[#91EAE4] text-transparent bg-clip-text bg-gradient-to-r drop-shadow-2xl'>I'm Kevin Paul</div>
       <div className='grid gap-4 grid-cols-2 grid-rows-2 w-max my-5'>
-        <div className='flex items-center'><AiOutlineGithub/>&nbsp;Github</div>
-        <div className='flex items-center'><AiOutlineLinkedin/>&nbsp;Linkedin</div>
-        <div className='flex items-center'><AiOutlineTwitter/>&nbsp;Twitter</div>
-        <div className='flex items-center'><AiOutlineInstagram/>&nbsp;Instagram</div>
+        <a className='flex items-center' href="https://github.com/itaintkevin"><AiOutlineGithub/>&nbsp;Github</a>
+        <a className='flex items-center' href="https://www.linkedin.com/in/itaintkevin"><AiOutlineLinkedin/>&nbsp;Linkedin</a>
+        <a className='flex items-center' href="https://twitter.com/it_aint_kevin"><AiOutlineTwitter/>&nbsp;Twitter</a>
+        <a className='flex items-center' href="https://www.instagram.com/it_aint_kevin"><AiOutlineInstagram/>&nbsp;Instagram</a>
       </div>
       <div className='flex justify-center w-full my-5'>
         <div className='bg-[#91EAE4] absolute text-md rounded-xl mx-5 p-5'>
@@ -73,13 +73,13 @@ const Hero = () => {
               Computer Science Engineering student at SRMIST, India.🏛️
         </div>
         <div className='bg-[#2c2f3d] translate-y-8 absolute text-md rounded-xl drop-shadow-2xl mx-5 p-5'>
-              Curently learning Backend Web Developement.🤌
+              ~ Curently learning Backend Web Developement 🤌.
               <br/>
-              Exploring Web3 and Blockchain Technology.⛓️
+              ~ Exploring Web3 and Blockchain Technology ⛓️.
               <br/>
-              Technical Team member at IEEE & TEDx SRMIST.💻
+              ~ Technical Team member at IEEE & TEDx SRMIST 💻.
               <br/>
-              Computer Science Engineering student at SRMIST, India.🏛️
+              ~ Computer Science Engineering student at SRMIST, India 🏛️.
         </div>
       </div>
     </div>
